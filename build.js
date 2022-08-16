29d758be580acd7285c8e4d6e57ddcd3fe5b9f3b
lfijhi <lfijhi@norsys.fr>
2022-08-16 10:56:37 +0100

    my business-card lfijhi.

--- build.js
+++ build.js
@@ -15,21 +15,17 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('           Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  shorthandle: chalk.white('bnb'),
-  work: chalk.white('Staff Developer Advocate at Twilio'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  name: chalk.white('FIJHI Lahoucine'),
+  handle: chalk.white('lfijhi'),
+  shorthandle: chalk.white(''),
+  work: chalk.white('Norsys web developer'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~lfijhi'),
+  github: chalk.gray('https://github.com/') + chalk.green('houcin175'),
+  web: chalk.cyan('norsys.fr'),
+  npx: chalk.red('npx') + ' ' + chalk.white('lfijhi'),
   labelWork: chalk.white.bold('    Work:'),
-  labelTwitter: chalk.white.bold(' Twitter:'),
   labelnpm: chalk.white.bold('     npm:'),
   labelGitHub: chalk.white.bold('  GitHub:'),
-  labelLinkedIn: chalk.white.bold('LinkedIn:'),
   labelWeb: chalk.white.bold('     Web:'),
   labelCard: chalk.white.bold('    Card:')
 }
@@ -38,10 +34,8 @@ const data = {
 const newline = '\n'
 const heading = `${data.name} / ${data.handle} / ${data.shorthandle}`
 const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
@@ -49,10 +43,8 @@ const carding = `${data.labelCard}  ${data.npx}`
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
-               twittering + newline + // data.labelTwitter + data.twitter
                npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
                webing + newline + newline + // data.labelWeb + data.web
                carding // data.labelCard + data.npx
 
